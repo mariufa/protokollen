3897bde2acac8185b0124d55f88a63559819ad66
Marius Fagerland <mariusfagerland@yahoo.no>
2018-06-14 03:58:57 +0200

    change table entry

--- client/src/Common/TextCommons.js
+++ client/src/Common/TextCommons.js
@@ -2,11 +2,15 @@ import React from 'react';
 import styled from 'styled-components';
 
 export const TableEntry = styled.div`
+    outline: none;
+    display: block;
+    background-color: inherit;
     padding: 10px;
     margin 10px auto;
     border-radius: 5px;
     border: 1px solid #ccc;
     cursor: pointer;
+    width: 100%;
 `;
 
 export const TextBox = styled.p`

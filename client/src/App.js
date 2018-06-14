ad0af3f41e459ea0449dbe0d17b38fc558e56198
Marius Fagerland <mariusfagerland@yahoo.no>
2018-06-14 03:58:39 +0200

    set browserrouter

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { HashRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import styled from 'styled-components';
 import AuthComponent from './User/AuthComponent';
 import RequireAuth from './User/RequireAuth';

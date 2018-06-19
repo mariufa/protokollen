d713b1e0a3aafbbcc4d520f3a8fcb80183cd5d88
Marius Fagerland <mariusfagerland@yahoo.no>
2018-06-19 04:13:40 +0200

    Implement create label

--- client/src/Protocols/Labels.js
+++ client/src/Protocols/Labels.js
@@ -1,38 +1,97 @@
-import React from 'react';
-import styled from 'styled-components';
+import React, { Component } from "react";
+import styled from "styled-components";
 
 const LabelsBox = styled.div`
-    margin: 10px;
-    display: float;
+  margin: 10px;
+  overflow: hidden;
 `;
 
 const LabelName = styled.div`
-    margin: 0 10px;
-    padding: 5px 10px;
-    color: white;
-    background-color: midnightblue;
-    border-radius: 5px;
+  margin: 10px 10px;
+  padding: 5px 10px;
+  color: white;
+  background-color: midnightblue;
+  border-radius: 5px;
+  float: left;
 `;
 
 const Icon = styled.span`
-    margin-left: 10px;
+  margin-left: 10px;
 `;
 
-const Labels = ({ labels, removable }) => {
-    return (
-        <LabelsBox>
-            {labels.map(label =>
-                <LabelName key={label._id}>
-                    {label.name}
-                    {removable && 
+const CreateLabelName = LabelName.extend`
+  color: midnightblue;
+  background-color: white;
+  border: 1px dotted midnightblue;
+`;
+
+const InputLabel = styled.input`
+    margin: 0px 1px;
+    outline: none;
+    box-sizing: border-box;
+`;
+
+class CreateLabel extends Component {
+
+    constructor(props) {
+        super(props);
+        this.addLabel = props.addLabel;
+        this.state = {
+            lableInputTextOpen : false
+        };
+    }
+
+    onCreate = () => {
+        this.setState({lableInputTextOpen: true});
+    }
+
+    onEnter = (e) => {
+        if (e.key === 'Enter') {
+            console.log(e.target.value)
+            this.addLabel(e.target.value);
+            this.setState({lableInputTextOpen: false})
+        }
+    }
+
+    render() {
+        return (
+            <div>
+            { !this.state.lableInputTextOpen &&
+                <CreateLabelName onClick={this.onCreate}>
+                    Create
                     <Icon>
-                        <i className="fa fa-times" aria-hidden="true"></i>
+                        <i className="fa fa-plus" aria-hidden="true" />
                     </Icon>
-                    }
-                </LabelName>
-            )}
-        </LabelsBox>
-    );
+                </CreateLabelName>
+            }
+            { this.state.lableInputTextOpen &&
+                <CreateLabelName>
+                    <InputLabel onKeyPress={this.onEnter}></InputLabel>
+                </CreateLabelName>
+            }
+            </div>
+            
+        
+        );
+    }
 }
+
+const Labels = ({ labels, editable, addLabel }) => {
+  return (
+    <LabelsBox>
+      {labels.map(label => (
+        <LabelName key={label._id}>
+          {label.name}
+          {editable && (
+            <Icon>
+              <i className="fa fa-times" aria-hidden="true" />
+            </Icon>
+          )}
+        </LabelName>
+      ))}
+      {editable && <CreateLabel addLabel={addLabel} />}
+    </LabelsBox>
+  );
+};
 
 export default Labels;
--- client/src/Protocols/Protocol.js
+++ client/src/Protocols/Protocol.js
@@ -1,138 +1,150 @@
-import React, { Component } from 'react';
-import styled from 'styled-components';
+import React, { Component } from "react";
+import styled from "styled-components";
 
-import Labels from './Labels';
-import HorizontalDivider from '../Common/Divider';
-import { TableEntry, TextBox } from '../Common/TextCommons';
-import { CommonButton } from '../Common/Button';
-import Page from '../Common/Page';
+import Labels from "./Labels";
+import HorizontalDivider from "../Common/Divider";
+import { TableEntry, TextBox } from "../Common/TextCommons";
+import { CommonButton } from "../Common/Button";
+import Page from "../Common/Page";
 
 const TextArea = styled.textarea`
-    border: 1px solid #ccc;
-    border-radius: 5px;
-    width: 100%;
-    outline: none;
-    resize: none;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  width: 100%;
+  outline: none;
+  resize: none;
 `;
 
 const SaveBox = styled.div`
-    width: 100%;
-    display: inline-block;
+  width: 100%;
+  display: inline-block;
 `;
 
 const SaveButton = CommonButton.extend`
-    float:right
+  float: right;
 `;
 
 class Protocol extends Component {
+  state = {
+    _id: "",
+    text: "",
+    labels: [],
+    comments: [],
+    removable: false,
+    textEditable: false
+  };
 
-    state = {
-        _id: '',
-        text: '',
-        labels: [],
-        comments: [],
-        removable: false,
-        textEditable: false
+  getMockData = id => {
+    if (id === "1") {
+      return {
+        _id: "1",
+        text: "hei hå test blalsls",
+        labels: [
+          {
+            _id: "1",
+            name: "label A"
+          },
+          {
+            _id: "2",
+            name: "label B"
+          }
+        ]
+      };
     }
+    return {};
+  };
 
-    getMockData = (id) => {
-        if (id === "1") {
-            return {
-                _id: "1",
-                text: "hei hå test blalsls",
-                labels: [
-                    {
-                        _id: "1",
-                        name: "label A"
-                    },
-                    {
-                        _id: "2",
-                        name: "label B"
-                    }
-                ]
-            };
+  getMockComments = id => {
+    if (id === "1") {
+      return [
+        {
+          _id: "1",
+          text: "mornings mann",
+          protocolId: "1",
+          created: "blabla",
+          edited: "",
+          user: "Marius Fagerland"
+        },
+        {
+          _id: "2",
+          text: "mornings mann 2",
+          protocolId: "1",
+          created: "blabla",
+          edited: "",
+          user: "Espen"
         }
-        return {};
+      ];
     }
+    return [];
+  };
 
-    getMockComments = (id) => {
-        if (id === "1") {
-            return [{
-                _id: "1",
-                text: "mornings mann",
-                protocolId: "1",
-                created: "blabla",
-                edited: '',
-                user: "Marius Fagerland"
-            },
-            {
-                _id: "2",
-                text: "mornings mann 2",
-                protocolId: "1",
-                created: "blabla",
-                edited: '',
-                user: "Espen"
-            }]
-        }
-        return [];
-    }
+  componentDidMount = () => {
+    const id = this.props.match.params.id;
+    const protocol = this.getMockData(id);
+    const comments = this.getMockComments(id);
+    this.setState({ ...protocol, comments: comments });
+  };
 
-    componentDidMount = () => {
-        const id = this.props.match.params.id;
-        const protocol = this.getMockData(id);
-        const comments = this.getMockComments(id);
-        this.setState({...protocol, comments: comments});
+  onClickText = () => {
+    if (this.state.textEditable === false) {
+      this.setState({ textEditable: true });
     }
+  };
 
-    onClickText = () => {
-        if (this.state.textEditable === false) {
-            this.setState({textEditable: true});
-        }
-    }
+  onClickLabels = () => {
+    this.setState({ removable: !this.state.removable });
+  };
 
-    onClickLabels = () => {
-        this.setState({removable: !this.state.removable});
-    }
+  onClickSaveText = () => {
+    // put api
+    this.setState({ textEditable: false });
+  };
 
-    onClickSaveText = () => {
-        // put api
-        this.setState({textEditable: false});
-    }
+  onChangeTextArea = e => {
+    this.setState({ text: e.target.value });
+  };
 
-    onChangeTextArea = (e) => {
-        this.setState({text: e.target.value});
+  addLabel = labelText => {
+    if (labelText && labelText.length > 0) {
+      let labels = this.state.labels;
+      const newLabel = {
+        _id: "123",
+        name: labelText
+      };
+      labels.push(newLabel);
+      this.setState({ labels: labels });
     }
+  };
 
-    render() {
-        const { text, labels, textEditable } = this.state;
-        return (
-            <Page>
-                <TableEntry onClick={this.onClickText}>
-                    {!textEditable &&
-                        <TextBox>
-                            {text}
-                        </TextBox>
-                    }
-                    {textEditable &&
-                        <div>
-                            <TextArea value={text} onChange={this.onChangeTextArea} wrap="off" rows="3" />
-                            <SaveBox>
-                                <SaveButton onClick={this.onClickSaveText}>Save</SaveButton>
-                            </SaveBox>
-                        </div>
-                    }
-                </TableEntry>
-                <HorizontalDivider/>
-                <TableEntry onClick={this.onClickLabels}>
-                    <Labels labels={labels} removable={this.state.removable}/>
-                </TableEntry>
-                <HorizontalDivider/>
-                <CommonButton>
-                    Comments
-                </CommonButton>
-            </Page>
-        );
-    }
+  render() {
+    const { text, labels, textEditable } = this.state;
+    return (
+      <Page>
+        <TableEntry onClick={this.onClickText}>
+          {!textEditable && <TextBox>{text}</TextBox>}
+          {textEditable && (
+            <div>
+              <TextArea
+                value={text}
+                onChange={this.onChangeTextArea}
+                wrap="off"
+                rows="3"
+              />
+              <SaveBox>
+                <SaveButton onClick={this.onClickSaveText}>Save</SaveButton>
+              </SaveBox>
+            </div>
+          )}
+        </TableEntry>
+        <HorizontalDivider />
+        <TableEntry onClick={this.onClickLabels}>
+          <Labels labels={labels} editable={true} addLabel={this.addLabel} />
+        </TableEntry>
+        <HorizontalDivider />
+        <CommonButton>Comments</CommonButton>
+      </Page>
+    );
+  }
 }
 
 export default Protocol;

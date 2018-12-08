73f86360cfa38628407b2341a93cb54dee790aac
Marius Fagerland <mariusfagerland@yahoo.no>
2018-12-08 14:22:33 +0100

    Add Spring-boot initializer

---
+++ server-java/src/main/java/com/fagerland/protokollen/ProtokollenApplication.java
@@ -0,0 +1,12 @@
+package com.fagerland.protokollen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProtokollenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProtokollenApplication.class, args);
+	}
+}
